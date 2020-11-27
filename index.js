296bdbf65f5e74474c752177d33b4de8d3a5dbc5
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-27 14:53:27 -0300

    config MySQL

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
 
-const app = customExpress();
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+conexao.connect(erro => {
+    if (erro) {
+        console.log(erro, 'Não está funcionando');
+    } else {
+        console.log('Conectado com sucesso!');
+        const app = customExpress();
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    }
+});
