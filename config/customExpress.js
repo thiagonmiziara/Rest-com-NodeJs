503e68ac4d903bb551b098e83c02da05dd126620
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-26 21:44:09 -0300

    add metodo post

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const consign = require('consign');
+const express = require('express')
+const consign = require('consign')
+const bodyParser = require('body-parser')
 
 module.exports = () => {
-    const app = express();
+    const app = express()
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
 
     consign()
         .include('controllers')
-        .into(app);
+        .into(app)
 
-    return app;
+    return app
 }
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,11 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'));
+    // crianto um get
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+        // criando um post
+    app.post('/atendimentos', (req, res) => {
+
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e está realizando um POST')
+
+    })
 }
