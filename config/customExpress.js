7fe9445b9dfd3356c4f615d254762d9979931420
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-30 13:31:30 -0300

    resolvendo o cors

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 module.exports = () => {
     const app = express()
 
-    app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,PATCH,POST,DELETE')
+        app.use(cors())
+        next()
+    })
 
     consign()
         .include('controllers')
